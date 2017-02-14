a59ce07342b40b42ee2ec60bdd29ad4cf4248aaa
smunter6 <SMunter@kaman.com>
2017-02-14 13:39:07 -0800

    reactive slider

--- layout.py
+++ layout.py
@@ -74,7 +74,7 @@ class ShowcaseApp(App):
             #tc1 = self.dmc.GCommand('TC1')
             print (e , ': ' + tc1)
             #self.dmc.GClose()
-            self.root.ids.avTitle.title = tc1#Update title with error message
+            self.root.ids.avTitle.title = e#Update title with error message
             #self.root.ids.sm.switch_to(self.firstScreen)
 
     #This function will update the homing screen UI elements.
